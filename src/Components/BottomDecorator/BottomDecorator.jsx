2939c7c1079d870518311d55b3d7b39529a6802f
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-01-02 17:33:57 +0530

    feat(assets/components)bottomDecorator component added

---
+++ src/Components/BottomDecorator/BottomDecorator.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './BottomDecroator.css'
+
+function BottomDecorator(bottom, left) {
+  return (
+    <div className='bottom-decorator-layout'>
+        <div className='box-one'></div>
+        <div className='box-two'></div>
+        <div className='box-three'></div>
+        <div className='circle'></div>
+    </div>
+  )
+}
+
+export default BottomDecorator
