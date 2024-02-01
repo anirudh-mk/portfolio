682376c7d5dfdae2aff2c3b7dbdc9b1c1ec8a39b
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-01 18:07:29 +0530

    optim(IconButton)

--- src/Components/IconButton/IconButton.jsx
+++ src/Components/IconButton/IconButton.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import './IconButton.css'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./IconButton.css";
 
-function IconButton({icon, link, icon_button_color, icon_color, top, left}) {
+function IconButton({ icon, link, icon_button_color, icon_color }) {
   return (
-      <div className="icon-button" style={{backgroundColor:icon_button_color, top:top, left:left}}>
-        <a href={link} target="_blank" rel="noopener noreferrer">
-         <FontAwesomeIcon icon={icon} size="lg" style={{color:icon_color, paddingTop:'8'}} />
-        </a>
-      </div>
-  )
+    <div
+      className="flex items-center justify-center icon-button"
+      style={{
+        backgroundColor: icon_button_color ? icon_button_color : "#fff",
+      }}
+    >
+      <a href={link} target="_blank" rel="noopener noreferrer">
+        <FontAwesomeIcon
+          icon={icon}
+          size="lg"
+          style={{ color: icon_color ? icon_color : "#fff" }}
+        />
+      </a>
+    </div>
+  );
 }
 
-export default IconButton
+export default IconButton;
