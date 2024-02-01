ffb9e9e0a1974780ce1a0f6754ca49ac40de7cf8
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-01 17:57:55 +0530

    optim:(TextButton)

--- src/Components/TextButton/TextButton.jsx
+++ src/Components/TextButton/TextButton.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import './TextButton.css'
+import React from "react";
+import "./TextButton.css";
 
-function TextButton({button_text, link, text_button_color, font_color, top, left}) {
+function TextButton({ button_text, link, text_button_color, font_color }) {
   return (
-    <div className='text-button' style={{backgroundColor:text_button_color, top:top, left:left}}>
-        <a href={link}>
-          <p style={{color:font_color}}>{button_text}</p>
-        </a>
+    <div
+      className="flex items-center justify-center text-button"
+      style={{
+        backgroundColor: text_button_color ? text_button_color : "#fff",
+      }}
+    >
+      <a href={link}>
+        <p style={{ color: font_color ? font_color : "#fff" }}>{button_text}</p>
+      </a>
     </div>
-  )
+  );
 }
 
-export default TextButton
+export default TextButton;
