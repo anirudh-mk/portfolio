198d9cefb946799b39c74b8bef0aa64226039cf8
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-01 23:40:03 +0530

    optim(BottomDecoration)

--- src/Components/BottomDecorator/BottomDecorator.jsx
+++ src/Components/BottomDecorator/BottomDecorator.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import './BottomDecroator.css'
+import "./BottomDecroator.css";
 
-function BottomDecorator(bottom, left) {
+function BottomDecorator({ bottom, left }) {
   return (
-    <div className='bottom-decorator-layout'>
-        <div className='box-one'></div>
-        <div className='box-two'></div>
-        <div className='box-three'></div>
-        <div className='circle'></div>
+    <div className="bottom-decorator-layout">
+      <div className="box-one"></div>
+      <div className="box-two"></div>
+      <div className="box-three"></div>
+      <div className="circle"></div>
     </div>
-  )
+  );
 }
 
-export default BottomDecorator
+export default BottomDecorator;
