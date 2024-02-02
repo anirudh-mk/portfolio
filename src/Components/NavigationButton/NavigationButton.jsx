a269e5188b068b01a71486839f8ab2bf07d07707
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-02 10:32:32 +0530

    optim(NavigationButton)

--- src/Components/NavigationButton/NavigationButton.jsx
+++ src/Components/NavigationButton/NavigationButton.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import './NavigationButton.css'
+import React from "react";
+import "./NavigationButton.css";
 
-function NavigationButton({href, title, width, top, left, background}) {
+function NavigationButton({ href, title, width, background }) {
   return (
-    <div>
-      <a href={href}>
-        <div className="navigation-button" style={{width:width, top:top, left:left, background:background}}>
-            <p>{title}</p>
-        </div>
-      </a>
-    </div>
-  )
+    <a href={href}>
+      <div
+        className="navigation-button"
+        style={{ width: width, background: background }}
+      >
+        <p>{title}</p>
+      </div>
+    </a>
+  );
 }
 
-export default NavigationButton
+export default NavigationButton;
