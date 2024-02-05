8c2f4ae7b5c49bc4d81cce2a1869b9948991fe8a
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-02-05 23:02:17 +0530

    feat(some changes)

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -64,22 +64,9 @@ function App() {
           </div> */}
       {/* </div>
       </div> */}
-
-      <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="https://flowbite.com/"
-            class="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              class="h-8"
-              alt="Flowbite Logo"
-            />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
-            </span>
-          </a>
+      <nav class="navbar lg:w-[1192px]">
+        <div class="flex flex-wrap items-center justify-between mx-auto p-4">
+          <HeaddingText href="#Home" headding="Anirudh MK" />
           <button
             data-collapse-toggle="navbar-default"
             type="button"
@@ -105,47 +92,43 @@ function App() {
             </svg>
           </button>
           <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="flex flex-col  md:flex-row items-center  ">
               <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  Home
-                </a>
+                <NavigationButton
+                  href="#home"
+                  title="Home"
+                  width={115}
+                  top={33}
+                  left={588}
+                  background="rgba(255, 255, 255, 0.50)"
+                />
               </li>
               <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  About
-                </a>
+                <NavigationButton
+                  href="#about"
+                  title="About"
+                  width={115}
+                  top={33}
+                  left={730}
+                />
               </li>
               <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Services
-                </a>
+                <NavigationButton
+                  href="#project"
+                  title="Project"
+                  width={115}
+                  top={33}
+                  left={872}
+                />
               </li>
               <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Pricing
-                </a>
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Contact
-                </a>
+                <NavigationButton
+                  href="#contact"
+                  title="Contact Me"
+                  width={149}
+                  top={33}
+                  left={1014}
+                />
               </li>
             </ul>
           </div>
